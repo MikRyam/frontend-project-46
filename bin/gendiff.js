34edb18c1b8dce5816464b0254fd00d8e455be67
mika <mikhailryamov@gmail.com>
2022-09-03 18:58:40 +0300

    make Step_2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,12 +2,13 @@
 
 import { Command } from 'commander';
 
-// const { Command } = require('commander');
 const program = new Command();
 
 program
-  .name('genDiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .description('Compares two configuration files and shows a difference')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+
 
 program.parse();

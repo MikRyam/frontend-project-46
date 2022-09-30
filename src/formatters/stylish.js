4f3dcc1aa3f1fc1e6825540578611b460bf3803c
mika <mikhailryamov@gmail.com>
2022-09-30 14:39:34 +0300

    modify stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,44 +1,34 @@
 import _ from 'lodash';
 
-const getDepth = (depth) => ' '.repeat(depth * 2);
+const getDepth = (depth, indent = 4) => ' '.repeat(depth * indent - 2);
 
 const getValue = (data, depth = 0) => {
   if (!_.isPlainObject(data) || data === null) {
     return String(data);
   }
   const entries = Object.entries(data);
-  const items = entries.map(([key, value]) => `${getDepth(depth + 1)}  ${key}: ${getValue(value, depth + 2)}`);
-  return `{\n${items.join('\n')}\n${getDepth(depth)}}`;
+  const values = entries.map(([key, value]) => `${getDepth(depth)}  ${key}: ${getValue(value, depth + 1)}`);
+  return `{\n${values.join('\n')}\n${getDepth(depth - 1)}  }`;
 };
 
-const operatorsMap = {
-  unchanged: ' ',
-  added: '+',
-  changed: '-',
-  removed: '-',
-};
-
-const makeStylish = (data, depth = 0) => {
-  const result = data.map((item) => {
-    const {
-      key, children, value, value1, value2, type,
-    } = item;
-    switch (type) {
+const makeStylish = (diff) => {
+  const iter = (data, depth) => data.map((node) => {
+    switch (node.type) {
       case 'unchanged':
-        return `${getDepth(depth + 1)}  ${key}: ${getValue(value, depth + 2)}`;
+        return `${getDepth(depth)}  ${node.key}: ${getValue(node.value, depth + 1)}`;
       case 'added':
-        return `${getDepth(depth + 1)}${operatorsMap.added} ${key}: ${getValue(value, depth + 2)}`;
+        return `${getDepth(depth)}+ ${node.key}: ${getValue(node.value, depth + 1)}`;
       case 'removed':
-        return `${getDepth(depth + 1)}${operatorsMap.removed} ${key}: ${getValue(value, depth + 2)}`;
+        return `${getDepth(depth)}- ${node.key}: ${getValue(node.value, depth + 1)}`;
       case 'changed':
-        return `${getDepth(depth + 1)}${operatorsMap.changed} ${key}: ${getValue(value1, depth + 2)}\n${getDepth(depth + 1)}${operatorsMap.added} ${key}: ${getValue(value2, depth + 2)}`;
+        return `${getDepth(depth)}- ${node.key}: ${getValue(node.value1, depth + 1)}\n${getDepth(depth)}+ ${node.key}: ${getValue(node.value2, depth + 1)}`;
       case 'nested':
-        return `${getDepth(depth + 1)}  ${key}: ${makeStylish(children, depth + 2)}`;
+        return `${getDepth(depth)}  ${node.key}: {\n${iter(node.children, depth + 1).join('\n')}\n${getDepth(depth)}  }`;
       default:
-        throw new Error(`${type} - is unknown type`);
+        throw new Error(`${node.type} - is unknown type`);
     }
   });
-  return `{\n${result.join('\n')}\n${getDepth(depth)}}`;
+  return `{\n${iter(diff, 1).join('\n')}\n}`;
 };
 
 export default makeStylish;

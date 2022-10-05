da0a51029424411bc5243792d24af9842aa679b3
mika <mikhailryamov@gmail.com>
2022-10-05 11:36:38 +0300

    fix bugs, round2

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -13,16 +13,30 @@ const expectedStylish = readFile('expectedStylish.txt');
 const expectedPlain = readFile('expectedPlain.txt');
 const expectedJson = readFile('expectedJson.txt');
 
-const extensions = ['json', 'yml', 'yaml'];
+const cases = [
+  ['file1.json', 'file2.json'],
+  ['file1.yaml', 'file2.yaml', 'stylish'],
+  ['file1.json', 'file2.json', 'plain'],
+  ['file1.yaml', 'file2.yaml', 'plain'],
+  ['file1.json', 'file2.json', 'json'],
+  ['file1.yaml', 'file2.yaml', 'json'],
+];
 
 describe('All tests', () => {
-  test.each(extensions)('Format %s', (extension) => {
-    const file1 = getFixturePath(`file1.${extension}`);
-    const file2 = getFixturePath(`file2.${extension}`);
-
-    expect(gendiff(file1, file2)).toEqual(expectedStylish);
-    expect(gendiff(file1, file2, 'stylish')).toEqual(expectedStylish);
-    expect(gendiff(file1, file2, 'plain')).toEqual(expectedPlain);
-    expect(gendiff(file1, file2, 'json')).toEqual(expectedJson);
+  test.each(cases)('Format %s', (file1, file2, format = 'stylish') => {
+    const actual = gendiff(getFixturePath(file1), getFixturePath(file2), format);
+    const expected = (style) => {
+      switch (style) {
+        case 'stylish':
+          return expectedStylish;
+        case 'plain':
+          return expectedPlain;
+        case 'json':
+          return expectedJson;
+        default:
+          return expectedStylish;
+      }
+    };
+    expect(actual).toEqual(expected(format));
   });
 });

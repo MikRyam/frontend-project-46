d4ae186cb283d1031760bb17a3a14e903cc918d4
mika <mikhailryamov@gmail.com>
2022-10-13 12:22:42 +0300

    fix yml parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,12 @@
 import yaml from 'js-yaml';
+import yml from "js-yaml";
 
 const parse = (data, format) => {
   switch (format) {
     case 'json':
       return JSON.parse(data);
     case 'yml':
+      return yml.load(data);
     case 'yaml':
       return yaml.load(data);
     default:
